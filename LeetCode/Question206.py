03b13c8fcf266bf4f3c41815828c6492aaac2231
Amit Kumar Shaw <shawamitkumar76@gmail.com>
2026-09-15 11:03:22 +0530

    Add new coding solutions

---
+++ LeetCode/Question206.py
@@ -0,0 +1,19 @@
+'''
+Given the head of a singly linked list, reverse the list
+and return the new head.
+'''
+
+# Approach: Three Pointer
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
